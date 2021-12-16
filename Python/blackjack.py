da8237de9b21cf6d89f798516a74e256d554fe8b
migimigster <raymund_munchua@dlsu.edu.ph>
2021-12-16 17:47:03 +0800

    added doubledown feature

--- Python/blackjack.py
+++ Python/blackjack.py
@@ -1,7 +1,6 @@
 import random
 import os
 
-
 def deal_card():
     '''This is where cards are randomly chosen from the list'''
     cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
@@ -26,24 +25,7 @@ def clearConsole():
     if os.name in ('nt', 'dos'):
         command = 'cls'
     os.system(command)
-
-def comparison(playerscore,cpuscore,cash,betamount):
-    '''Comparing the scores on final tally'''
-    if playerscore==21 or cpuscore>21 or (playerscore>cpuscore and playerscore<21):
-        print("You win!")
-        cash+=betamount
-        print(f"Your remaining cash is ${cash}")
-        return
-    elif playerscore==cpuscore:
-        print("It's a draw")
-        print(f"Your remaining cash is ${cash}")
-        return
-    elif playerscore>21 or cpuscore>21 or (playerscore<cpuscore and cpuscore<21):
-        print("You lose")
-        cash-=betamount
-        print(f"Your remaining cash is ${cash}")
-        return
-     
+    
 from blackjackart import logo
 
 def game(cash):
@@ -55,6 +37,7 @@ def game(cash):
         print(f"Your remaining cash is ${cash}")
         player_hand = []
         cpu_hand = []
+        ddown=0
         program_done=False
 
         #This is where cards are append on player and cpu's hand. Also user will input bet amount
@@ -68,11 +51,15 @@ def game(cash):
             #This is where player and cpu hands' are shown
             player_score=calculate_score(player_hand)
             cpu_score=calculate_score(cpu_hand)
-            print(f"Player's hand is {player_hand} equal to {player_score}")
+            print(f"\nPlayer's hand is {player_hand} equal to {player_score}")
             print(f"CPU's hand is {cpu_hand} equal to {cpu_score}")
 
             #If initial hand is 21 then game is already over
-            if player_score==21 or cpu_score>21:
+            if player_score==cpu_score and player_score==21:
+                print("It's a draw")
+                print(f"Your remaining cash is ${cash}")
+                break
+            elif player_score==21 or cpu_score>21:
                 print("You win!")
                 cash+=bet_amount
                 print(f"Your remaining cash is ${cash}")
@@ -83,11 +70,14 @@ def game(cash):
                 print(f"Your remaining cash is ${cash}")
                 break
             else:
-                #This is where players can chose to hit another card
-                add_card=input("Take a hit? y or n? ")
+                #This is where players choose to hit, doubledown, split(coming soon) or stand
+                add_card=input("Hit: y\nDouble Down: d (Note that if you hit once, you cannot double down!)\nStand: n\n\n")
+        #-----HIT! - This is where players can chose to hit another card----------------------------------#
             if add_card =='y':
                 player_hand.append(deal_card())
-            else:
+                ddown+=1 # To make sure that you cannot double down once hit
+        #-----STAND! - This is where players stand with computer----------------------------------#
+            elif add_card == 'n':
                 #This is where cpu will hit if hand is less than 17
                 while cpu_score!=0 and cpu_score<17:
                     cpu_hand.append(deal_card())
@@ -95,8 +85,45 @@ def game(cash):
                 print(f"Player's hand is {player_hand} equal to {player_score}")
                 print(f"CPU's hand is {cpu_hand} equal to {cpu_score}")
                 program_done=True
+
                 #This is where comparison occurs after hitting
-                comparison(player_score,cpu_score,cash,bet_amount)
+                if  cpu_score>21 or (player_score>cpu_score and player_score<=21):
+                    print("You win!")
+                    cash+=bet_amount
+                    print(f"Your remaining cash is ${cash}")                    
+                elif player_score==cpu_score:
+                    print("It's a draw")
+                    print(f"Your remaining cash is ${cash}")                    
+                elif player_score>21 or (player_score<cpu_score and cpu_score<=21):
+                    print("You lose")
+                    cash-=bet_amount
+                    print(f"Your remaining cash is ${cash}")
+        #-----DOUBLE DOWN! - This is where players will hit another card then their bet amount is increased 2x----------------------------------#
+            elif add_card == 'd' and ddown==0:
+                bet_amount*=2
+                player_hand.append(deal_card())
+                player_score=calculate_score(player_hand)
+            #This is where cpu will hit if hand is less than 17
+                while cpu_score!=0 and cpu_score<17:
+                    cpu_hand.append(deal_card())
+                    cpu_score=calculate_score(cpu_hand)
+                print(f"Player's hand is {player_hand} equal to {player_score}")
+                print(f"CPU's hand is {cpu_hand} equal to {cpu_score}")
+                program_done=True
+
+                #This is where comparison occurs after hitting
+                if player_score>21 or (player_score<cpu_score and cpu_score<=21):
+                    print("You lose")
+                    cash-=bet_amount
+                    print(f"Your remaining cash is ${cash}")
+                elif  cpu_score>21 or (player_score>cpu_score and player_score<=21):
+                    print("You win!")
+                    cash+=bet_amount
+                    print(f"Your remaining cash is ${cash}")                   
+                elif player_score==cpu_score:
+                    print("It's a draw")
+                    print(f"Your remaining cash is ${cash}")                    
+
         #when game is over, program asks user if u want to bet again using remaining cash
         if cash==0 or cash<0:
             game_overall = True
@@ -121,5 +148,3 @@ while not overall:
         overall = True
     else:
         clearConsole()
-
-
