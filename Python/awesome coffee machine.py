e7d0a4f33796dc6b405b5974a1a5fc58854dd1e2
migimigster <raymund_munchua@dlsu.edu.ph>
2021-12-29 20:16:47 +0800

    cleaner code

--- Python/awesome coffee machine.py
+++ Python/awesome coffee machine.py
@@ -1,103 +1,23 @@
-from coffeemachinedata import menu,resources
-import os
+from coffeemachinedata import menu,resources,menu_interface,clearConsole,print_interface,coffee,minus_inventory,refill_inventory
 real_program_done = False
-
-def clearConsole():
-    '''Clearing the console'''
-    command = 'clear'
-    if os.name in ('nt', 'dos'):
-        command = 'cls'
-    os.system(command)
-
-def money_count(orders,quantity):
-    print("Please insert coins to pay!\n")
-    while True:
-        try:
-            c_quarter=float(input("How many quarters? "))*0.25
-            break
-        except ValueError:
-            print("Please input valid number!")
-    while True:
-        try:
-            c_dime=float(input("How many dimes? "))*0.10
-            break
-        except ValueError:
-            print("Please input valid number!")
-    while True:
-        try:
-            c_nickel=float(input("How many nickels? "))*0.05
-            break
-        except ValueError:
-            print("Please input valid number!")
-    while True:
-        try:
-            c_penny=float(input("How many pennies? "))*0.01
-            break
-        except ValueError:
-            print("Please input valid number!")
-    total_money = round((c_quarter+c_dime+c_nickel+c_penny)-(orders['cost'])*quantity,2)
-    if total_money<0:
-        return total_money
-    else:
-        print(f'\nI received ${round(c_quarter+c_dime+c_nickel+c_penny,2)}')
-        print(f'Your change is: ${total_money}')
-        return total_money
-
-def minus_inventory(orders,inventory,quantity):
-    rem_water=int(inventory['water']-(orders['ingredients']['water'])*quantity)
-    rem_coffee=int(inventory['coffee']-(orders['ingredients']['coffee'])*quantity)
-    rem_milk=int(inventory['milk']-(orders['ingredients']['milk']*quantity))
-    sales=float(round(inventory['cash']+orders['cost'],2))*quantity
-    new_inventory = {
-        "water": rem_water,
-        "milk": rem_milk,
-        "coffee": rem_coffee,
-        "cash": sales
-    }
-    if new_inventory["water"]<0:
-        print("Not enough water!")
-        return inventory
-    elif new_inventory["coffee"]<0:
-        print("Not enough coffee!")
-        return inventory
-    elif new_inventory["milk"]<0:
-        print("Not enough milk!")
-        return inventory
-    else:
-        clearConsole()
-        print(f'You ordered {quantity} cups of {coffee_order}')
-        print(f'Your bill is ${orders["cost"]*quantity}')
-        cash=money_count(order,order_quantity)
-        if cash<0:
-            print("You do not have enough money!!")
-            return inventory
-        else:
-            if quantity>1:
-                print(f'Here are your {coffee_order}s ☕! Enjoy!')
-                return new_inventory
-            else:
-                print(f'Here is your {coffee_order} ☕! Enjoy!')
-                return new_inventory
 while not real_program_done:
 
     program_done = False
     clearConsole()
     print("WELCOME TO MY AWESOME COFFEE MACHINE")
     while not program_done:
-        cash=0
-        coffee_order = input("What would you like to order?\nEspresso: $1.50\nLatte: $2.50\nCappuccino: $3.00\nTo turn off the machine, please type 'off'\n\n").lower()
+
+        coffee_order = menu_interface()
         if coffee_order == 'report':
-            clearConsole()
-            print(f'Water: {resources["water"]} ml')
-            print(f'Coffee: {resources["coffee"]} g')
-            print(f'Milk: {resources["milk"]} ml')
-            print(f'Sales: ${resources["cash"]}')
-            input("Press any key to continue ")
+            print_interface(resources)
+            input("")
             program_done = True
         elif coffee_order == 'off':
             program_done = True
             real_program_done = True
-        elif coffee_order == 'espresso' or coffee_order=='latte' or coffee_order=='cappuccino':
+        elif coffee_order == '1' or coffee_order=='2' or coffee_order=='3' or coffee_order=='4':
+            coffee_order=int(coffee_order)
+            coffee_order=coffee(coffee_order)
             while True:
                 try:
                     order_quantity=int(input(f'How many orders do you want for {coffee_order}? '))
@@ -106,16 +26,18 @@ while not real_program_done:
                     clearConsole()
                     print("Please input valid number! ")
             order = menu[f'{coffee_order}']
-            resources=minus_inventory(order,resources,order_quantity)
+            resources=minus_inventory(order,resources,order_quantity,coffee_order)
             press_any_key=input("Press enter to order again! If you do not want to order again please type off\n")
             if press_any_key=='off':
                 program_done = True
                 real_program_done = True
             else:
                 program_done=True
+        elif coffee_order=='refill':
+            refill_inventory(resources)
         else:
-            print("\nSorry! I don't know that")
-            input("Press enter to continue")
+            input("\nSorry! I don't know that")
             program_done = True
 clearConsole()
-print('Thank you for using the machine! Good-bye')
+input('Thank you for using the machine! Good-bye! ')
+clearConsole()
--- Python/coffeemachinedata.py
+++ Python/coffeemachinedata.py
@@ -1,3 +1,4 @@
+import os
 menu = {
     "espresso": {
         "ingredients": {
@@ -22,6 +23,14 @@ menu = {
             "coffee": 24,
         },
         "cost": 3.0,
+    },
+    "americano": {
+        "ingredients": {
+            "water": 300,
+            "milk": 75,
+            "coffee": 32,
+        },
+        "cost": 3.5,
     }
 }
 
@@ -31,4 +40,117 @@ resources = {
     "coffee": 10000,
     "cash": 0
 }
+def coffee(customer_order):
+    if customer_order==1:
+        return 'espresso'
+    elif customer_order==2:
+        return 'latte'
+    elif customer_order==3:
+        return 'cappuccino'
+    elif customer_order==4:
+        return 'americano'
+def clearConsole():
+    '''Clearing the console'''
+    command = 'clear'
+    if os.name in ('nt', 'dos'):
+        command = 'cls'
+    os.system(command)
+def print_interface(resources):
+    clearConsole()
+    print(f'Water: {resources["water"]} ml')
+    print(f'Coffee: {resources["coffee"]} g')
+    print(f'Milk: {resources["milk"]} ml')
+    print(f'Sales: ${resources["cash"]}')
+def money_count(orders,quantity):
+    print("Please insert coins to pay!\n")
+    while True:
+        try:
+            c_quarter=float(input("How many quarters? "))*0.25
+            break
+        except ValueError:
+            print("Please input valid number!")
+    while True:
+        try:
+            c_dime=float(input("How many dimes? "))*0.10
+            break
+        except ValueError:
+            print("Please input valid number!")
+    while True:
+        try:
+            c_nickel=float(input("How many nickels? "))*0.05
+            break
+        except ValueError:
+            print("Please input valid number!")
+    while True:
+        try:
+            c_penny=float(input("How many pennies? "))*0.01
+            break
+        except ValueError:
+            print("Please input valid number!")
+    total_money = round((c_quarter+c_dime+c_nickel+c_penny)-(orders['cost'])*quantity,2)
+    if total_money<0:
+        return total_money
+    else:
+        print(f'\nI received ${round(c_quarter+c_dime+c_nickel+c_penny,2)}')
+        print(f'Your change is: ${total_money}')
+        return total_money
+def minus_inventory(orders,inventory,quantity,coffee_order):
+    rem_water=int(inventory['water']-(orders['ingredients']['water'])*quantity)
+    rem_coffee=int(inventory['coffee']-(orders['ingredients']['coffee'])*quantity)
+    rem_milk=int(inventory['milk']-(orders['ingredients']['milk']*quantity))
+    sales=float(round(inventory['cash']+orders['cost'],2))*quantity
+    new_inventory = {
+        "water": rem_water,
+        "milk": rem_milk,
+        "coffee": rem_coffee,
+        "cash": sales
+    }
+    if new_inventory["water"]<0:
+        print("Not enough water!")
+        return inventory
+    elif new_inventory["coffee"]<0:
+        print("Not enough coffee!")
+        return inventory
+    elif new_inventory["milk"]<0:
+        print("Not enough milk!")
+        return inventory
+    else:
+        clearConsole()
+        print(f'You ordered {quantity} cups of {coffee_order}')
+        print(f'Your bill is ${orders["cost"]*quantity}')
+        cash=money_count(orders,quantity)
+        if cash<0:
+            print("You do not have enough money!!")
+            return inventory
+        else:
+            if quantity>1:
+                print(f'Here are your {coffee_order}s ☕! Enjoy!')
+                return new_inventory
+            else:
+                print(f'Here is your {coffee_order} ☕! Enjoy!')
+                return new_inventory
+def refill_inventory(inventory):
+    clearConsole()
+    refill_ing=input("Which ingredient would you like to refill? Water? Coffee? Milk?\n").lower()
+    if refill_ing=='water':
+        quan=int(input("How many milliliters of water? "))
+        inventory['water']=int(inventory['water']+quan)
+    elif refill_ing=='coffee':
+        quan=int(input("How many grams of coffee? "))
+        inventory['coffee']=int(inventory['coffee']+quan)
+    elif refill_ing=='milk':
+        quan=int(input("How many milliliters of milk? ")) 
+        inventory['milk']=int(inventory['milk']+quan)
+    else:
+        clearConsole()
+        return inventory
+    print(f'Water: {inventory["water"]} ml')
+    print(f'Coffee: {inventory["coffee"]} g')
+    print(f'Milk: {inventory["milk"]} ml')
+    input("Press enter to continue")
+    clearConsole()
+    return inventory
 
+def menu_interface():
+    x=input("What would you like to order? (Input their respective number.\n1. Espresso: $1.50\n2. Latte: $2.50\n3. Cappuccino: $3.00\n4. Americano: $3.50\nTo turn off the machine, please type 'off'\n\n").lower()
+    return x
